825a7292cb5e4738f470dde98478068621b055a0
git <dima.it.ua@gmail.com>
2023-09-20 09:54:47 +0300

    cart/CartContextProvider update
    cart - new totalAmount var using Math f-n + addCartItemHandler & removeCartItemHandler
    CartContextProvider return code for remove item

--- src/Layout/HeaderCartButton.js
+++ src/Layout/HeaderCartButton.js
@@ -1,16 +1,31 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import CartIcon from "../Cart/CartIcon";
 import styles from "./HeaderCartButton.module.css";
 import CartContext from "../store/cart-context";
 
 const HeaderCartButton = (props) => {
+  const [buttonAnimated, setButtonAnimated] = useState(false);
   const cartContext = useContext(CartContext);
   const cartItemAmount = cartContext.items.reduce((currentValue, item) => {
     return currentValue + item.amount;
   }, 0);
 
+  const buttonClasses = `${styles.button} ${buttonAnimated ? styles.bump : ""}`;
+
+  useEffect(() => {
+    if (cartContext.items.length === 0) {
+      return;
+    }
+    setButtonAnimated(true);
+    const timer = setTimeout(() => {
+      setButtonAnimated(false);
+    }, 300);
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [cartContext.items]);
   return (
-    <button className={styles.button} onClick={props.onClick}>
+    <button className={buttonClasses} onClick={props.onClick}>
       <span className={styles.icon}>
         <CartIcon />
       </span>
--- src/store/CartContextProvider.js
+++ src/store/CartContextProvider.js
@@ -7,14 +7,11 @@ const defaultCartState = {
 };
 
 const CartReducer = (state, action) => {
-  console.log(action);
-  console.log(action.item.amount);
-
   if (action.type === "ADD_ITEM") {
     const updatedTotalAmount =
       state.totalAmount + action.item.price * action.item.amount;
-    
-    const existingCartItemIndex = state.items.findIndex(item => {
+
+    const existingCartItemIndex = state.items.findIndex((item) => {
       return item.id === action.item.id;
     });
 
@@ -25,7 +22,7 @@ const CartReducer = (state, action) => {
 
     if (existingCartItem) {
       updatedItem = {
-        ...updatedItem,
+        ...existingCartItem,
         amount: existingCartItem.amount + action.item.amount,
       };
       updatedItems = [...state.items];
@@ -34,7 +31,30 @@ const CartReducer = (state, action) => {
       updatedItem = { ...action.item };
       updatedItems = state.items.concat(updatedItem);
     }
-     
+
+    return {
+      items: updatedItems,
+      totalAmount: updatedTotalAmount,
+    };
+  }
+  if (action.type === "REMOVE_ITEM") {
+    const existingCartItemIndex = state.items.findIndex((item) => {
+      return item.id === action.id;
+    });
+    const existingCartItem = state.items[existingCartItemIndex];
+    const updatedTotalAmount = state.totalAmount - existingCartItem.price;
+
+    let updatedItems;
+    if (existingCartItem === 1) {
+      updatedItems = state.items.filter((item) => item.id !== action.id);
+    } else {
+      const updatedItem = {
+        ...existingCartItem,
+        amount: existingCartItem.amount - 1,
+      };
+      updatedItems = [...state.items];
+      updatedItems[existingCartItemIndex] = updatedItem;
+    }
     return {
       items: updatedItems,
       totalAmount: updatedTotalAmount,
